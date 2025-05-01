7217232a087ed342d440899130731da8e6fecbfb
Ella R <ellaraputri@gmail.com>
2025-05-02 01:27:34 +0700

    feat: reconfigure to mysql and create models

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import User from './models/User.js';
+import Task from './models/Task.js';
+import { sequelize, testConnection } from './config/db.js';
 
 dotenv.config();
 
@@ -10,10 +13,21 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/', (req, res) => {
   res.send('Hello world !');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.use('/api/task', taskRouter)
+// app.use('/api/user', userRouter)
+
+const startApp = async () => {
+  await testConnection(); 
+  await sequelize.sync({ alter: true });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startApp();
